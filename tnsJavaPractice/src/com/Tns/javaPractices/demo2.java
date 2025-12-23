ba5bf4b8bc90fb590825e74edce8aad9c7ec9442
Bharath-Git125 <bharathdumpala33@gmail.com>
2025-12-23 12:09:56 +0530

    2nd demo (add,sub)

---
+++ tnsJavaPractice/src/com/Tns/javaPractices/demo2.java
@@ -0,0 +1,12 @@
+package com.Tns.javaPractices;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b + "addition");
+		System.out.println(b-a+"subtraction");
+	}
+
+}
