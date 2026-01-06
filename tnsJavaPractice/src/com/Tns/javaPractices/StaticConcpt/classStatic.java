8920439706475ba4b28ba823892b630c9f9f4c0c
Bharath-Git125 <bharathdumpala33@gmail.com>
2026-01-06 19:58:34 +0530

    static concept java

---
+++ tnsJavaPractice/src/com/Tns/javaPractices/StaticConcpt/classStatic.java
@@ -0,0 +1,18 @@
+package com.Tns.javaPractices.StaticConcpt;
+
+
+public class classStatic {
+	public static void main(String[] args) {
+		System.out.println(staticexplain.a);	
+		staticexplain.show();
+	}
+	class staticexplain{	
+		static int a = 10;//variable
+		static void show() {//method
+			System.out.println("hello world");
+		}
+		static {//block
+			System.out.println("yoooo!");
+		}
+	}
+}
