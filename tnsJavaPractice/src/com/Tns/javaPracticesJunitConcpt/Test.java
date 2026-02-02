a1eb1461145d35a627cae6de178cdaa82f7e13a7
Bharath-Git125 <bharathdumpala33@gmail.com>
2026-02-02 11:58:04 +0530

    TNS java practice

---
+++ tnsJavaPractice/src/com/Tns/javaPracticesJunitConcpt/Test.java
@@ -0,0 +1,5 @@
+package com.Tns.javaPracticesJunitConcpt;
+
+public @interface Test {
+
+}
---
+++ tnsJavaPractice/src/com/Tns/javaPracticesJunitConcpt/junitDemo.java
@@ -0,0 +1,14 @@
+package com.Tns.javaPracticesJunitConcpt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class junitDemo {
+
+	@Test
+	void test() {
+		System.out.println("Not yet implemented");
+	}
+
+}
